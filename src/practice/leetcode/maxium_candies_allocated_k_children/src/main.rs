8085b1121dc8344e8b631944e8e05c9919396b1c
vuquang23 <levuquang2k1@gmail.com>
2025-03-15 21:50:50 +0700

    practice leetcode - maximum candies allocated to k children

---
+++ src/practice/leetcode/maxium_candies_allocated_k_children/src/main.rs
@@ -0,0 +1,12 @@
+struct Solution;
+
+impl Solution {
+    pub fn maximum_candies(candies: Vec<i32>, k: i64) -> i32 {
+        10
+    }
+}
+
+fn main() {
+    let result = Solution::maximum_candies(vec![5, 8, 6], 3);
+    println!("{result}")
+}
